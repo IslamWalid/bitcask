ee8ca063bdf2ac3168058e0f9d900c677286e47c
IslamWalid <islamwalid83@gmail.com>
2022-11-02 20:13:28 +0200

    Add more test functions

--- bitcask_test.go
+++ bitcask_test.go
@@ -1,8 +1,11 @@
 package bitcask
 
 import (
+	"fmt"
 	"os"
 	"path"
+	"reflect"
+	"strconv"
 	"testing"
 )
 
@@ -89,6 +92,162 @@ func TestOpen(t *testing.T) {
 
 		assertError(t, err, want)
 		os.RemoveAll("no open dir")
+	})
+}
+
+func TestGet(t *testing.T) {
+	t.Run("get existing value", func(t *testing.T) {
+		b, _ := Open(testBitcaskPath, ReadWrite, SyncOnPut)
+		b.Put("key12", "value12345")
+
+		got, _ := b.Get("key12")
+		want := "value12345"
+
+		assertString(t, got, want)
+		b.Close()
+		os.RemoveAll(testBitcaskPath)
+	})
+
+	t.Run("get not existing value", func(t *testing.T) {
+		b, _ := Open(testBitcaskPath, ReadWrite)
+
+		want := "unknown key: key does not exist"
+		_, err := b.Get("unknown key")
+
+		assertError(t, err, want)
+		os.RemoveAll(testBitcaskPath)
+	})
+}
+
+func TestPut(t *testing.T) {
+	t.Run("put values with writer permission", func(t *testing.T) {
+		b, _ := Open(testBitcaskPath, ReadWrite)
+		b.Put("key12", "value12345")
+
+		want := "value12345"
+		got, _ := b.Get("key12")
+
+		assertString(t, got, want)
+		os.RemoveAll(testBitcaskPath)
+	})
+
+	t.Run("put with no write permission", func(t *testing.T) {
+		b1, _ := Open(testBitcaskPath, ReadWrite)
+		b1.Close()
+
+		b2, _ := Open(testBitcaskPath)
+		err := b2.Put("key12", "value12345")
+
+		assertError(t, err, "Put: require write permission")
+		os.RemoveAll(testBitcaskPath)
+	})
+}
+
+func TestDelete(t *testing.T) {
+	t.Run("delete existing key", func(t *testing.T) {
+		b, _ := Open(testBitcaskPath, ReadWrite, SyncOnPut)
+		b.Put("key12", "value12345")
+		b.Delete("key12")
+		_, err := b.Get("key12")
+		assertError(t, err, "key12: key does not exist")
+		os.RemoveAll(testBitcaskPath)
+	})
+
+	t.Run("delete not existing key", func(t *testing.T) {
+		b, _ := Open(testBitcaskPath, ReadWrite, SyncOnDemand)
+		err := b.Delete("key12")
+		assertError(t, err, "key12: key does not exist")
+		os.RemoveAll(testBitcaskPath)
+	})
+
+	t.Run("delete with no write permission", func(t *testing.T) {
+		b1, _ := Open(testBitcaskPath, ReadWrite)
+		b1.Close()
+
+		b2, _ := Open(testBitcaskPath)
+		err := b2.Delete("key12")
+		assertError(t, err, "Delete: require write permission")
+		os.RemoveAll(testBitcaskPath)
+	})
+
+	t.Run("check if loaded delete is detected", func(t *testing.T) {
+		b1, _ := Open(testBitcaskPath, ReadWrite, SyncOnPut)
+		b1.Put("key12", "value12345")
+		b1.Delete("key12")
+		b1.Close()
+
+		b2, _ := Open(testBitcaskPath, ReadWrite, SyncOnPut)
+		_, err := b2.Get("key12")
+		want := "key12: key does not exist"
+		assertError(t, err, want)
+		os.RemoveAll(testBitcaskPath)
+	})
+}
+
+func TestListkeys(t *testing.T) {
+	b, _ := Open(testBitcaskPath, ReadWrite, SyncOnDemand)
+
+	key := "key12"
+	value := "value12345"
+	b.Put(key, value)
+
+	want := []string{"key12"}
+	got := b.ListKeys()
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got:\n%v\nwant:\n%v", got, want)
+	}
+	os.RemoveAll(testBitcaskPath)
+}
+
+func TestFold(t *testing.T) {
+	b, _ := Open(testBitcaskPath, ReadWrite, SyncOnDemand)
+
+	for i := 0; i < 10; i++ {
+		b.Put(fmt.Sprint(i+1), fmt.Sprint(i+1))
+	}
+
+	want := 110
+	got := b.Fold(func(s1, s2 string, a any) any {
+		acc, _ := a.(int)
+		k, _ := strconv.Atoi(s1)
+		v, _ := strconv.Atoi(s2)
+
+		return acc + k + v
+	}, 0)
+
+	if got != want {
+		t.Errorf("got:%d, want:%d", got, want)
+	}
+	os.RemoveAll(testBitcaskPath)
+}
+
+func TestMerge(t *testing.T) {
+
+}
+
+func TestSync(t *testing.T) {
+	t.Run("put with sync on demand option is set", func(t *testing.T) {
+		b, _ := Open(testBitcaskPath, ReadWrite)
+		b.Put("key12", "value12345")
+		b.Sync()
+
+		want := "value12345"
+		got, _ := b.Get("key12")
+
+		assertString(t, got, want)
+		os.RemoveAll(testBitcaskPath)
+	})
+
+	t.Run("sync with no write permission", func(t *testing.T) {
+		b1, _ := Open(testBitcaskPath, ReadWrite)
+		b1.Close()
+
+		b2, _ := Open(testBitcaskPath)
+		err := b2.Sync()
+
+		assertError(t, err, "Sync: require write permission")
+		os.RemoveAll(testBitcaskPath)
 	})
 }
 
